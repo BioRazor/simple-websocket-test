99807fcc48e489f72c753d9a24119cca807337b6
David Abreu <biorazor.a@gmail.com>
2020-08-19 23:31:19 -0400

    replace room_name by group_name

--- prueba/ws_test/consumers.py
+++ prueba/ws_test/consumers.py
@@ -7,23 +7,23 @@ class TestConsumer(JsonWebsocketConsumer):
     """ Create the Seller room, and is the only available to send messages to client"""
 
     def connect(self):
-        self.room_name = "test_room"
+        self.group_name = "test_room"
 
         async_to_sync(self.channel_layer.group_add)(
-            self.room_name,
+            self.group_name,
             self.channel_name
         )
         self.accept()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
-            self.room_name,
+            self.group_name,
             self.channel_name
         )
 
     def receive_json(self, content):
         async_to_sync(self.channel_layer.group_send)(
-            self.room_name, {
+            self.group_name, {
                 'type': 'echo_message',
                 'product': content
             }
